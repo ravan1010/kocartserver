d442da1ebd28f5a6548af03f6fc6551bfd3ee344
ravan1010 <ravanravana177@gmail.com>
2026-03-03 08:55:26 +0530

    main

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ const URL = process.env.MONGODB_URL ;
 
 const dbconnection = async () => {
 
-mongoose.connect('mongodb://localhost:27017/food')
+mongoose.connect(URL)
 .then(() => console.log("✅ MongoDB connected")) 
 .catch((err) => console.error("❌ MongoDB error:", err));
 
