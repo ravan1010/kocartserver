c0891c6deb7bd12ffb4f9cb707eac568dcc0572e
ravan1010 <ravanravana177@gmail.com>
2026-07-13 23:13:00 +0530

    main

--- middleware/admin_auth.js
+++ middleware/admin_auth.js
@@ -77,4 +77,28 @@ export const admincat = async (req, res, next) => {
 }
 
 
+//app
+export const appAdminAuth = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({
+      message: "No token",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.admingu = decoded;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+};
+
+
 
--- router/admin_router.js
+++ router/admin_router.js
@@ -4,27 +4,48 @@ import express from 'express';
 
 import { signat } from '../middleware/OGauth.js';
 const router = express.Router();
-import { admingu, adminif, admintoa } from '../middleware/admin_auth.js';
+import { admingu, adminif, admintoa, appAdminAuth } from '../middleware/admin_auth.js';
 import { AdminFCMtoken, Adminid, admininfo, bookedlisttoadmin, dashboard, EVENTCreate, EVENTDelete, EVENTUpdate, getAdminOrders, getSinglePost, open, openORclose, Toadmin, updateOrder } from '../controller/admin_controller.js';
 
 ///admin
 
 router.route('/admin/info').post(signat, admingu, admininfo)
-
+//web
 router.route('/adminid').get(admingu, Adminid);
+//app
+router.route('/app/adminid').get(appAdminAuth, Adminid);
 
+
+//web
 router.route('/marchent/fcmToken').post(admingu, AdminFCMtoken)
+
+//app
+router.route('/app/marchent/fcmToken').post(appAdminAuth, AdminFCMtoken)
+
+
 
 //product  
 router.route('/admin/post').post(admingu, EVENTCreate  )
 router.route("/admin/post/:id").put(admingu, EVENTUpdate);
 router.route("/admin/post/:id").get(admingu, getSinglePost);
-
+//web
 router.route('/admin/post/open/:id').post(admingu, open)
 
+//app
+router.route('/app/admin/post/open/:id').post(appAdminAuth, open)
 
+
+
+//web
 router.route('/admin/dashboard').get(admingu, dashboard )
+//app
+router.route('/app/admin/dashboard').get(appAdminAuth, dashboard )
+
+//web
 router.route('/admin/door').post(admingu, openORclose)
+//app
+router.route('/app/admin/door').post(appAdminAuth, openORclose)
+
 router.route('/admin/:id').delete(admingu, EVENTDelete)
 
 
