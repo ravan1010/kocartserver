244af3902f8ad7efd2784942f2c4b6d0f09f30ff
ravan1010 <ravanravana177@gmail.com>
2026-07-15 03:20:22 +0530

    main

--- utils/firebase.js
+++ utils/firebase.js
@@ -24,78 +24,78 @@ admin.initializeApp({
     }),
   });
 
-export const sendPushNotification = async (fcmToken, title, body, url) => {
-  if (!fcmToken || typeof fcmToken !== "string") {
-    console.log("⚠️ FCM token missing, push skipped");
-    return;
-  }
-
-  const message = {
-    token: fcmToken,
-
-    notification: {
-      title,
-      body,
-    },
-
-    data: {
-      screen: "Orders",
-      orderId: orderId.toString(),
-      url,
-    },
-
-    android: {
-      priority: "high",
-      notification: {
-        channelId: "default",
-      },
-    },
-
-    webpush: {
-      fcmOptions: {
-        link: url,
-      },
-      notification: {
-        title,
-        body,
-      },
-    },
-  };
-
-  try {
-    const response = await admin.messaging().send(message);
-    console.log("✅ Push sent:", response);
-    return response;
-  } catch (error) {
-    console.error("❌ FCM Error:", error);
-  }
-};
-
 // export const sendPushNotification = async (fcmToken, title, body, url) => {
-
-//   if (typeof fcmToken !== "string") {
+//   if (!fcmToken || typeof fcmToken !== "string") {
 //     console.log("⚠️ FCM token missing, push skipped");
 //     return;
 //   }
 
-//  const massage = {
-//   token: fcmToken,
-//   notification: {
-//     title,
-//     body,
-//   },
-//   webpush: {
-//     fcmOptions: {
-//     link: url,
-//   },
-//   },
-// }; 
+//   const message = {
+//     token: fcmToken,
+
+//     notification: {
+//       title,
+//       body,
+//     },
+
+//     data: {
+//       screen: "Orders",
+//       orderId: orderId.toString(),
+//       url,
+//     },
+
+//     android: {
+//       priority: "high",
+//       notification: {
+//         channelId: "default",
+//       },
+//     },
+
+//     webpush: {
+//       fcmOptions: {
+//         link: url,
+//       },
+//       notification: {
+//         title,
+//         body,
+//       },
+//     },
+//   };
 
 //   try {
-//     const res = await admin.messaging().send(massage);
-//     return res;
+//     const response = await admin.messaging().send(message);
+//     console.log("✅ Push sent:", response);
+//     return response;
 //   } catch (error) {
-//     // throw error;
 //     console.error("❌ FCM Error:", error);
 //   }
 // };
+
+export const sendPushNotification = async (fcmToken, title, body, url) => {
+
+  if (typeof fcmToken !== "string") {
+    console.log("⚠️ FCM token missing, push skipped");
+    return;
+  }
+
+ const massage = {
+  token: fcmToken,
+  notification: {
+    title,
+    body,
+  },
+  webpush: {
+    fcmOptions: {
+    link: url,
+  },
+  },
+}; 
+
+  try {
+    const res = await admin.messaging().send(massage);
+    return res;
+  } catch (error) {
+    // throw error;
+    console.error("❌ FCM Error:", error);
+  }
+};
