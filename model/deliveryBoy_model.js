0b6edd0a4f25f785937d0214629d3db076962853
ravan1010 <ravanravana177@gmail.com>
2026-07-25 18:30:52 +0530

    main

---
+++ model/deliveryBoy_model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const deliveryBoySchema = new mongoose.Schema({
+  googleId: String,
+  name: String,
+  email: String,
+  avatar: String,
+  Number: {
+    type: Number,
+  },
+  activate: {
+    type: Boolean,
+    default: false
+  },
+  kocartAmount:{
+    type: Number,
+    default: 0
+  },
+  city: String,
+  fcmToken: String,
+
+  isOnline: {
+    type: Boolean,
+    default: false
+  },
+
+  isAvailable: {
+    type: Boolean,
+    default: true
+  },
+
+  currentLocation: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point"
+    },
+    coordinates: {
+      type: [Number],
+      default: [0, 0]
+    }
+  }
+});
+
+deliveryBoySchema.index({
+  currentLocation: "2dsphere"
+});
+
+export default new mongoose.model("DeliveryBoy", deliveryBoySchema);
--- router/Ul.router.js
+++ router/Ul.router.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router()
 import {signat, authLocation, appAuth} from '../middleware/OGauth.js';
-import { home, setting, address, explore, cartdata, removecart, buy, order, addtocart, calculateDeliveryFee, nearby, updateLocation } from '../controller/UI.controller.js';
+import { home, setting, address, explore, cartdata, removecart, buy, order, addtocart, calculateDeliveryFee, nearby, updateLocation, merchantProducts } from '../controller/UI.controller.js';
 // const event_post_model = require('../model/event_post_model.js')
 import user_model from '../model/user_model.js';
 import { appplaceCODOrder, checkout, placeCODOrder, verifyPayment } from '../controller/orderController.js';
@@ -11,7 +11,7 @@ router.route('/liveupdate').post(liveupdate)
  
 router.route('/home').get( signat, home );
 router.put("/user/location", signat, updateLocation);
-router.get("/merchant/:id", homeAuth, merchantProducts);
+router.get("/merchant/:id", signat, merchantProducts);
 
 
 router.route('/explore').get( signat, explore )
