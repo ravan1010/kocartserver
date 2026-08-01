b0618041fd65a7b88c7817b0214f92135fc327f1
ravan1010 <ravanravana177@gmail.com>
2026-08-01 22:38:26 +0530

    main

--- router/log.js
+++ router/log.js
@@ -82,7 +82,7 @@ router.post("/app/google/user", async (req, res) => {
         }
         break;
 
-      case "parcelANDtransport":
+      case "parcelandtransport":
         account = await parcelANDtransport.findOne({ googleId });
 
         if (!account) {
