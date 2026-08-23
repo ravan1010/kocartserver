69c41d777d590c8c2c52da1c9b06127c5539d66a
ravan1010 <ravanravana177@gmail.com>
2026-08-23 14:33:02 +0530

    main

--- controller/parcelANDTransport.js
+++ controller/parcelANDTransport.js
@@ -410,7 +410,7 @@ export const getacceptedPendingOrders = async (req, res) => {
       });
     }
 
-    if(partner.isAvailable === true){
+    if(partner.isAvailable === false){
       return res.status(200).json({
             redirect: "assign",
             partner: partner.serviceType,
